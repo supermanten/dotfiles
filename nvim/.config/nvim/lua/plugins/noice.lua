86c0307db1d1cf3156f74d99e4262089d00750dc
Rio <rioten353@gmail.com>
2025-08-19 00:29:53 +0530

    Change: nvim config

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,7 +1,24 @@
 return {
 	"folke/noice.nvim",
-	event = "VeryLazy",
+	event = "VimEnter",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
 	opts = {
+		cmdline = {
+			enabled = true,
+			view = "cmdline_popup",
+			opts = { position = { row = 5, col = "50%" } },
+			format = {
+				cmdline = { pattern = "^:", icon = "", lang = "vim" },
+				search_down = { pattern = "^/", icon = " ", lang = "regex" },
+				search_up = { pattern = "^%?", icon = " ", lang = "regex" },
+				filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+				lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
+				help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+			},
+		},
 		routes = {
 			{
 				view = "notify",
@@ -36,6 +53,11 @@ return {
 			long_message_to_split = true, -- long messages will be sent to a split
 			inc_rename = true, -- enables an input dialog for inc-rename.nvim
 			lsp_doc_border = true, -- add a border to hover docs and signature help
+		},
+		status = {
+			command = {
+				hl_group = "NoiceStatusLine",
+			},
 		},
 	},
 }
