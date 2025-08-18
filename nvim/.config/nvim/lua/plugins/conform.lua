951dc0661b4ebc79b855a32b4172f68b235e1132
Rio <rioten353@gmail.com>
2025-08-19 00:03:14 +0530

    Change: nvim config

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,26 +1,52 @@
 return {
 	{
 		"stevearc/conform.nvim",
-		event = "BufWritePre", -- uncomment for format on save
-		lazy = true,
+		lazy = false,
+		keys = {
+			{
+				"<leader>fc",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = { "n", "v" },
+				desc = "Format buffer",
+			},
+		},
 		opts = {
 			formatters_by_ft = {
-
 				lua = { "stylua" },
 				dart = { "dartls" },
 				go = { "goimports", "gofumpt" },
-				python = { "ruff" },
+				python = { "ruff_format", "ruff_fix" },
 				cpp = { "clang_format" },
 				c = { "clang_format" },
-				-- css = { "prettier" },
-				-- html = { "prettier" },
-			},
 
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_fallback = true,
+				-- Expanded web development support
+				javascript = { "prettierd", "prettier" },
+				typescript = { "prettierd", "prettier" },
+				javascriptreact = { "prettierd", "prettier" },
+				typescriptreact = { "prettierd", "prettier" },
+				css = { "prettierd", "prettier" },
+				html = { "prettierd", "prettier" },
+				json = { "prettierd", "prettier" },
+				yaml = { "prettierd", "prettier" },
+				markdown = { "prettierd", "prettier" },
 			},
 		},
+		-- Enable format-on-save using an autocmd
+		config = function(_, opts)
+			require("conform").setup(opts)
+
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({
+						bufnr = args.buf,
+						timeout_ms = 500,
+						lsp_fallback = true,
+					})
+				end,
+			})
+		end,
 	},
 }
