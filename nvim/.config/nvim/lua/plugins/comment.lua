db80edd4d0f88ffdb0a5e296ede174389a7016d2
Rio <rioten353@gmail.com>
2025-08-18 23:52:59 +0530

    Change: nvim config

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -10,5 +10,10 @@ return {
 			block = "gob",
 		},
 	},
-	lazy = false,
+	keys = {
+		{ "gtc", mode = { "n", "v" } }, -- Toggler for lines
+		{ "gtb", mode = { "n", "v" } }, -- Toggler for blocks
+		{ "goc", mode = "n" }, -- Opleader for lines
+		{ "gob", mode = "n" }, -- Opleader for blocks
+	},
 }
