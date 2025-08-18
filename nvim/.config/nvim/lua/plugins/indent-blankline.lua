ec9e5b5f43f3ddc9556241559efdec54cfa6497b
Rio <rioten353@gmail.com>
2025-08-19 00:11:21 +0530

    Change: nvim config

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,13 +1,17 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
-	event = "VeryLazy",
+	event = { "BufReadPost", "BufNewFile" },
 	main = "ibl",
 	opts = {
 		indent = {
 			char = "│",
 			tab_char = "│",
 		},
-		scope = { enabled = false },
+		scope = {
+			enabled = true,
+			show_start = true,
+			show_end = true,
+		},
 		exclude = {
 			filetypes = {
 				"help",
