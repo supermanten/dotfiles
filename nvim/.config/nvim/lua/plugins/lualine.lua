e1c442e2d6b7929c2ddb2f6fcc3448c5f76ce239
Rio <rioten353@gmail.com>
2025-08-30 12:26:07 +0530

    Change: nvim  config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -212,6 +212,37 @@ return {
 					end,
 				},
 			},
+			winbar = {
+				lualine_a = {},
+				lualine_b = {
+					{
+						"filename",
+						path = 1, -- Show relative path
+						symbols = {
+							modified = "●",
+							readonly = "",
+							unnamed = "[No Name]",
+						},
+					},
+				},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+			inactive_winbar = {
+				lualine_a = {},
+				lualine_b = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
 		})
 	end,
 }
