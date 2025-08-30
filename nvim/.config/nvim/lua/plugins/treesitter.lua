54a524b02216b42e19e02f5df96113c582d49a32
Rio <rioten353@gmail.com>
2025-08-30 12:52:34 +0530

    Change: nvim  config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -9,6 +9,8 @@ return {
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
+				incremental_selection = { enable = true },
+				textobjects = { enable = true },
 			})
 		end,
 	},
