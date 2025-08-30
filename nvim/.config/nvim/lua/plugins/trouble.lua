f6e47c43a7ffc3d576f1634bd0db1dcbfb65f24b
Rio <rioten353@gmail.com>
2025-08-30 23:05:26 +0530

    Change: nvim trouble

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,60 +1,13 @@
+-- File: lua/plugins/trouble.lua
+
+local mappings = require("core.trouble.mappings")
+
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-	opts = {
-		focus = true,
-		auto_close = true,
-		position = "bottom",
-		padding = 2,
-		win = {
-			border = "rounded",
-		},
-		signs = {
-			error = "",
-			warning = "",
-			hint = "",
-			information = "",
-			other = "",
-		},
-	},
 	cmd = { "Trouble" },
-	keys = {
-		{ "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
-		{
-			"<leader>xd",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-			desc = "Open trouble document diagnostics",
-		},
-		{ "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
-		{ "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
-		{ "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
-		{
-			"[q",
-			function()
-				if require("trouble").is_open() then
-					require("trouble").prev({ skip_groups = true, jump = true })
-				else
-					local ok, err = pcall(vim.cmd.cprev)
-					if not ok then
-						vim.notify(err, vim.log.levels.ERROR)
-					end
-				end
-			end,
-			desc = "Previous Trouble/Quickfix Item",
-		},
-		{
-			"]q",
-			function()
-				if require("trouble").is_open() then
-					require("trouble").next({ skip_groups = true, jump = true })
-				else
-					local ok, err = pcall(vim.cmd.cnext)
-					if not ok then
-						vim.notify(err, vim.log.levels.ERROR)
-					end
-				end
-			end,
-			desc = "Next Trouble/Quickfix Item",
-		},
-	},
+	keys = mappings,
+	config = function()
+		require("core.trouble").setup()
+	end,
 }
