9d13d684b0015e5b4a4a914c9fa9e986207f94a7
Rio <rioten353@gmail.com>
2025-08-30 12:33:50 +0530

    Change: nvim  config

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -2,15 +2,22 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = { "BufReadPost", "BufNewFile" },
 	main = "ibl",
+	config = function()
+		-- Define highlights for catppuccin integration
+		vim.api.nvim_set_hl(0, "IblIndent", { fg = "#313244" }) -- Catppuccin surface0
+		vim.api.nvim_set_hl(0, "IblScope", { fg = "#f38ba8", bold = true }) -- Catppuccin red
+	end,
 	opts = {
 		indent = {
 			char = "│",
 			tab_char = "│",
+			highlight = "IblIndent",
 		},
 		scope = {
 			enabled = true,
 			show_start = true,
 			show_end = true,
+			highlight = "IblScope",
 		},
 		exclude = {
 			filetypes = {
