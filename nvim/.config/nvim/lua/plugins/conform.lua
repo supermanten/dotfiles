097b776b1255be387f291bac3d074776f3b2ef79
Rio <rioten353@gmail.com>
2025-08-30 23:00:58 +0530

    Change: nvim noe-tree

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,40 +1,19 @@
-return {
-	{
-		"stevearc/conform.nvim",
-		lazy = false,
-		keys = {
-			{
-				"<leader>fc",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = { "n", "v" },
-				desc = "Format buffer",
-			},
-		},
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-				go = { "goimports", "gofumpt" },
-				python = { "ruff_format", "ruff_fix" },
-				cpp = { "clang_format" },
-				c = { "clang_format" },
-			},
-		},
-		-- Enable format-on-save using an autocmd
-		config = function(_, opts)
-			require("conform").setup(opts)
+-- File: lua/plugins/conform.lua
 
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*",
-				callback = function(args)
-					require("conform").format({
-						bufnr = args.buf,
-						timeout_ms = 500,
-						lsp_fallback = true,
-					})
-				end,
-			})
-		end,
+return {
+	"stevearc/conform.nvim",
+	lazy = false,
+	keys = {
+		{
+			"<leader>fc",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = { "n", "v" },
+			desc = "Format buffer",
+		},
 	},
+	config = function()
+		require("core.conform").setup()
+	end,
 }
